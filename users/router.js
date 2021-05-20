99f9e4cf6d143b9ffd2c954ff13268168d0b222a
River <rivyyriv@gmail.com>
2021-05-19 22:07:22 -0600

    back-end complete

--- users/router.js
+++ users/router.js
@@ -1,105 +1,145 @@
-  
-const db = require('../data/dbConfig');
+const express = require('express');
+const Users = require('./users-model');
+const restricted = require('../auth/restricted-middleware');
 
-module.exports = {
-  addUser,
-  getUsers,
-  findBy,
-  getClassById,
-  getClassType,
-  getClass,
-  getIntensity,
-  getByLocation,
-  getByDuration,
-  getByInstructor,
-  addFavorite,
-  getFavoriteClass
-};
+const router = express.Router();
 
-function addUser(user) {
-  return db
-    .select('*')
-    .from('user')
-    .insert(user);
-}
+router.use(restricted);
 
-function getUserById(id) {
-  return db
-    .select('*')
-    .from('user')
-    .where({id});
-}
-
-function getUsers() {
-  return db.select('*').from('user');
-}
-
-function findBy(user) {
-  return db
-    .select('*')
-    .from('user')
-    .where(user);
-}
-
-function getClass() {
-  return db.select('*').from('class');
-}
-
-function getClassById(id) {
-  return db
-    .select('*')
-    .from('class')
-    .where({id})
-    .first();
-}
-
-function getClassType(type) {
-  return db
-    .select('*')
-    .from('class')
-    .where('class.type', '=', `${type}`);
-}
-
-function getIntensity(intensity) {
-  return db
-    .select('*')
-    .from('class')
-    .where({intensity});
-}
-
-function getByLocation(location) {
-  return db
-    .select('*')
-    .from('class')
-    .where({location});
-}
-
-function getByDuration(duration) {
-  return db
-    .select('*')
-    .from('class')
-    .where({duration});
-}
-
-function getByInstructor(instructor_name) {
-  return db
-    .select('*')
-    .from('class')
-    .where({instructor_name});
-}
-
-function addFavorite(user_id, class_id) {
-  return db('user_classes')
-    .insert({user_id, class_id})
-    .then(() => {
-      return getClassById(user_id);
+router.get('/', (req, res) => {
+  Users.getClass()
+    .then(classes => {
+      res.status(200).json({data: classes});
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Could not fetch classes', error: err.message});
     });
-}
+});
 
-function getFavoriteClass({user_id}) {
-  return db
-    .select('*')
-    .from('user_classes')
-    .join('user', 'user.id', 'user_classes.user_id')
-    .where('user.id', '=', `${user_id}`);
-}
+router.get('/type', (req, res) => {
+  const {type} = req.body;
+  // console.log(type);
+
+  Users.getClassType(type)
+    .then(clas => {
+      if (clas.length > 0) {
+        res.status(200).json({data: clas});
+      } else {
+        res.status(400).json({errr: 'there is a big error'});
+      }
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Error fetching type', error: err.message});
+    });
+});
+
+router.get('/intensity', (req, res) => {
+  const {intensity} = req.body;
+
+  Users.getIntensity(intensity)
+    .then(level => {
+      if (level.length > 0) {
+        res.status(200).json({data: level});
+      } else {
+        res.status(400).json({message: 'please choose between low, high or medium'});
+      }
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Failed to fetch data', error: err.message});
+    });
+});
+
+router.get('/location', (req, res) => {
+  const {location} = req.body;
+
+  Users.getByLocation(location)
+    .then(clas => {
+      if (clas) {
+        res.status(200).json({data: clas});
+      } else {
+        res.status(400).json({error: 'could not find location'});
+      }
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Error loading data', error: err.message});
+    });
+});
+
+router.get('/duration', (req, res) => {
+  const {duration} = req.body;
+
+  Users.getByDuration(duration)
+    .then(clas => {
+      if (clas) {
+        res.status(200).json({data: clas});
+      } else {
+        res.status(400).json({message: 'error'});
+      }
+    })
+    .catch(err => {
+      res.status(500).json({error: 'Error fetching data'});
+    });
+});
+
+router.get('/instructor', (req, res) => {
+  const {instructor_name} = req.body;
+
+  Users.getByInstructor(instructor_name)
+    .then(clas => {
+      if (clas) {
+        res.status(200).json({data: clas});
+      } else {
+        res.status(404).json({message: 'could not find class by instructor'});
+      }
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Error fetching instructor'});
+    });
+});
+
+router.post('/:id/favorite', (req, res) => {
+  const {class_id} = req.body;
+  const user_id = req.params.id;
+
+  Users.addFavorite(user_id, class_id)
+    .then(clas => {
+      if (clas) {
+        res.status(200).json({data: clas});
+      } else {
+        res.status(404).json({message: 'invalid id'});
+      }
+    })
+    .catch(err => {
+      res.status(500).json({message: err.message});
+    });
+});
+
+router.get('/:id/classes', (req, res) => {
+  const user_id = req.params.id;
+
+  Users.getFavoriteClass(user_id)
+    .then(clas => {
+      res.status(200).json({data: clas});
+    })
+    .catch(err => {
+      res.status(500).json({error: err.message});
+    });
+});
+
+// router.get('/:id', (req, res) => {
+//   const {id} = req.params;
+
+//   Users.getClassById(id)
+//     .then(clas => {
+//       if (clas) {
+//         res.status(200).json({data: clas});
+//       } else {
+//         res.status(404).json({error: 'please check the Id'});
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).json({message: 'Error getting class', error: err.message});
+//     });
+// });
+
+module.exports = router;
---
+++ utils/generateToken.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const secret = require('./vars');
+
+function generateToken(user) {
+  const payload = {
+    sub: user.id,
+    username: user.username,
+    role: user.role
+  };
+
+  const options = {
+    expiresIn: '1d'
+  };
+
+  return jwt.sign(payload, secret.jwtSecret, options);
+}
+
+module.exports = generateToken;
