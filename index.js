7be65b47e584ba5a45d50a94bc599d33e6f1c82b
JoshuaHolloway <jhollow6@asu.edu>
2021-05-24 13:01:12 -0500

    added some dependencies to get it running on my machine

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const server = require('./api/server');
 const port = process.env.PORT || 5000;
 
-server.listen(port, () => console.log(`Running server on port:${port}`));
+server.listen(port, () => console.log(`Running server on port:${port}  (Full URL with port # on MS Windows: http://localhost:${port}/)`));
